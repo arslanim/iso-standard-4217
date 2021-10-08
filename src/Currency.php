fbec027cd7bd94e94c09fde88cc0fd66907566c8
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-10-08 15:15:50 +0400

    Added constr.

--- src/Currency.php
+++ src/Currency.php
@@ -2,6 +2,10 @@
 
 namespace arslanimamutdinov\ISOStandard4217;
 
+use arslanimamutdinov\ISOStandard4217\exceptions\CurrencyStandardAlpha3EmptyException;
+use arslanimamutdinov\ISOStandard4217\exceptions\CurrencyStandardNameEmptyException;
+use arslanimamutdinov\ISOStandard4217\exceptions\CurrencyStandardNumericCodeEmptyException;
+
 class Currency
 {
     private $name;
@@ -10,6 +14,11 @@ class Currency
 
     public function __construct(string $name, string $alpha3, string $numericCode)
     {
+        $this->validateAttributes($name, $alpha3, $numericCode);
+
+        $this->name = $name;
+        $this->alpha3 = $alpha3;
+        $this->numericCode = $numericCode;
     }
 
     private function validateAttributes(string $name, string $alpha3, string $numericCode): void
