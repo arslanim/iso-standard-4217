9b10d7321ee30362ba3b2259a668c16c9079e003
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-10-08 15:10:48 +0400

    Added exception.

--- src/Currency.php
+++ src/Currency.php
@@ -7,4 +7,23 @@ class Currency
     private $name;
     private $alpha3;
     private $numericCode;
+
+    public function __construct(string $name, string $alpha3, string $numericCode)
+    {
+    }
+
+    private function validateAttributes(string $name, string $alpha3, string $numericCode): void
+    {
+        if (empty($name)) {
+            throw new CurrencyStandardNameEmptyException();
+        }
+
+        if (empty($alpha3)) {
+            throw new CurrencyStandardAlpha3EmptyException();
+        }
+
+        if (empty($numericCode)) {
+            throw new CurrencyStandardNumericCodeEmptyException();
+        }
+    }
 }
---
+++ src/exceptions/CurrencyStandardNameEmptyException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace arslanimamutdinov\ISOStandard4217\exceptions;
+
+use Exception;
+
+class CurrencyStandardNameEmptyException extends Exception
+{
+}
