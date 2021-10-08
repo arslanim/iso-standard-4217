e2e2bad0bead8a77c4e1ac686a5721d858fdf554
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-10-08 14:56:11 +0400

    Added unit test class.

---
+++ tests/unit/CurrencyTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace arslanimamutdinov\ISOStandard4217\tests\unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class CurrencyTest
+ * @package arslanimamutdinov\ISOStandard4217\tests\unit
+ * @group standard-4217
+ */
+class CurrencyTest extends TestCase
+{
+}
