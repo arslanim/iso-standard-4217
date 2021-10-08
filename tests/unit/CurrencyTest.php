c22d7d95d8cbc25e63bd9f5f5f23adae1f7a0896
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-10-08 15:00:30 +0400

    Covered success create.

--- tests/unit/CurrencyTest.php
+++ tests/unit/CurrencyTest.php
@@ -17,5 +17,10 @@ class CurrencyTest extends TestCase
 
     public function testSuccessCreate(): void
     {
+        $currency = new Currency(self::NAME_TEST_VALUE, self::ALPHA3_TEST_VALUE, self::NUMERIC_CODE_TEST_VALUE);
+
+        $this->assertEquals(self::NAME_TEST_VALUE, $currency->getName());
+        $this->assertEquals(self::ALPHA3_TEST_VALUE, $currency->getAlpha3());
+        $this->assertEquals(self::NUMERIC_CODE_TEST_VALUE, $currency->getNumericCode());
     }
 }
