4c33bb31ce043a0717f539ee26c4d93d0500f4b5
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-10-10 00:25:06 +0400

    Created function unit test class.

---
+++ tests/unit/functions/GetRawStandardsDataFunctionTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace arslanimamutdinov\ISOStandard4217\tests\unit\functions;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class GetRawStandardsDataFunctionTest
+ * @package arslanimamutdinov\ISOStandard4217\tests\unit\functions
+ * @group standard-4217-functions
+ */
+class GetRawStandardsDataFunctionTest extends TestCase
+{
+}
