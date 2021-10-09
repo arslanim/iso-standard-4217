9e046b0ecdf58b63cc9127a6153494d6a8879b4f
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-10-10 00:51:08 +0400

    Covered function.

--- tests/unit/functions/GetAllFunctionTest.php
+++ tests/unit/functions/GetAllFunctionTest.php
@@ -2,6 +2,10 @@
 
 namespace arslanimamutdinov\ISOStandard4217\tests\unit\functions;
 
+use arslanimamutdinov\ISOStandard4217\Currency;
+use arslanimamutdinov\ISOStandard4217\ISO4217;
+use arslanimamutdinov\ISOStandard4217\ISO4217Utility;
+use arslanimamutdinov\ISOStandardUtilities\codes\AttributeCodes;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -11,4 +15,42 @@ use PHPUnit\Framework\TestCase;
  */
 class GetAllFunctionTest extends TestCase
 {
+    public function testFunction(): void
+    {
+        $rawData = ISO4217::getRawStandardsData();
+
+        $utilityStandards = ISO4217::getAll();
+        $serviceStandards = (new ISO4217Utility())->getAll();
+
+        $this->assertStandardsEqualsRawData($utilityStandards, $rawData);
+        $this->assertStandardsEqualsRawData($serviceStandards, $rawData);
+        $this->assertUtilityStandardsEqualsServiceStandards($utilityStandards, $serviceStandards);
+    }
+
+    /**
+     * @param Currency[] $standards
+     * @param array $rawData
+     */
+    private function assertStandardsEqualsRawData(array $standards, array $rawData): void
+    {
+        $this->assertEquals(count($standards), count($rawData));
+
+        for ($i = 0; $i < count($rawData); $i++) {
+            $this->assertStandardEqualsRawData($standards[$i], $rawData[$i]);
+        }
+    }
+
+    private function assertUtilityStandardsEqualsServiceStandards(
+        array $utilityStandards,
+        array $serviceStandards
+    ): void {
+        $this->assertEquals($utilityStandards, $serviceStandards);
+    }
+
+    private function assertStandardEqualsRawData(Currency $currency, array $rawData): void
+    {
+        $this->assertEquals($rawData[AttributeCodes::ATTRIBUTE_NAME], $currency->getName());
+        $this->assertEquals($rawData[AttributeCodes::ATTRIBUTE_ALPHA3], $currency->getAlpha3());
+        $this->assertEquals($rawData[AttributeCodes::ATTRIBUTE_NUMERIC_CODE], $currency->getNumericCode());
+    }
 }
