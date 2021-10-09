2087cb52c7c2cf38bbc7483669ca80b8084de8c7
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-10-10 00:40:41 +0400

    Created unit test class.

---
+++ tests/unit/functions/GetAllFunctionTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace arslanimamutdinov\ISOStandard4217\tests\unit\functions;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class GetAllFunctionTest
+ * @package arslanimamutdinov\ISOStandard4217\tests\unit\functions
+ * @group standard-4217-functions
+ */
+class GetAllFunctionTest extends TestCase
+{
+}
