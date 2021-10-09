3a6775e26101b31bfedd923a93803578d65217eb
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-10-10 00:31:51 +0400

    Covered function.

--- tests/unit/functions/GetRawStandardsDataFunctionTest.php
+++ tests/unit/functions/GetRawStandardsDataFunctionTest.php
@@ -2,6 +2,9 @@
 
 namespace arslanimamutdinov\ISOStandard4217\tests\unit\functions;
 
+use arslanimamutdinov\ISOStandard4217\ISO4217;
+use arslanimamutdinov\ISOStandard4217\ISO4217Utility;
+use arslanimamutdinov\ISOStandardUtilities\codes\AttributeCodes;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -11,4 +14,28 @@ use PHPUnit\Framework\TestCase;
  */
 class GetRawStandardsDataFunctionTest extends TestCase
 {
+    public function testFunction(): void
+    {
+        $utilityRawStandardsData = ISO4217::getRawStandardsData();
+        $serviceRawStandardsData = (new ISO4217Utility())->getRawStandardsData();
+
+        $this->assertRawDataCorrect($utilityRawStandardsData);
+        $this->assertRawDataCorrect($serviceRawStandardsData);
+    }
+
+    private function assertRawDataCorrect(array $actualRawStandardsData): void
+    {
+        $this->assertNotEmpty($actualRawStandardsData);
+
+        foreach ($actualRawStandardsData as $actualRawStandardData) {
+            $this->assertRawStandardDataNotEmpty($actualRawStandardData);
+        }
+    }
+
+    private function assertRawStandardDataNotEmpty(array $actualRawStandardData): void
+    {
+        $this->assertNotEmpty($actualRawStandardData[AttributeCodes::ATTRIBUTE_NAME]);
+        $this->assertNotEmpty($actualRawStandardData[AttributeCodes::ATTRIBUTE_ALPHA3]);
+        $this->assertNotEmpty($actualRawStandardData[AttributeCodes::ATTRIBUTE_NUMERIC_CODE]);
+    }
 }
