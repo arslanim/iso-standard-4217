4b8a12648608ee2c5cac3370c3cb48fbf76b8bd8
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-10-12 00:30:57 +0400

    Created unit test class.

---
+++ tests/unit/functions/GetByNumericCodeFunctionTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace arslanimamutdinov\ISOStandard4217\tests\unit\functions;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class GetByNumericCodeFunctionTest
+ * @package arslanimamutdinov\ISOStandard4217\tests\unit\functions
+ * @group standard-4217-functions
+ */
+class GetByNumericCodeFunctionTest extends TestCase
+{
+}
