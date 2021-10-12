ce532f7f7368e42495b0f5dec9f140f0be606b7e
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-10-12 23:50:35 +0400

    Created test function.

---
+++ tests/unit/functions/GetAllByNumericCodesTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace arslanimamutdinov\ISOStandard4217\tests\unit\functions;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class GetAllByNumericCodesTest
+ * @package arslanimamutdinov\ISOStandard4217\tests\unit\functions
+ * @group standard-4217-functions
+ */
+class GetAllByNumericCodesTest extends TestCase
+{
+
+}
