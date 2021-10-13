a02ed70f430720cd577f680c9bad2a1efb72ff63
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-10-14 01:22:26 +0400

    Added test cases.

--- tests/unit/functions/GetAllByAlpha3CodesTest.php
+++ tests/unit/functions/GetAllByAlpha3CodesTest.php
@@ -2,6 +2,7 @@
 
 namespace arslanimamutdinov\ISOStandard4217\tests\unit\functions;
 
+use arslanimamutdinov\ISOStandardUtilities\codes\AttributeCodes;
 use PHPStan\Testing\TestCase;
 
 /**
@@ -9,4 +10,18 @@ use PHPStan\Testing\TestCase;
  */
 class GetAllByAlpha3CodesTest extends TestCase
 {
+    private const EXISTED_NUMERIC_CODE_VALUES = ['RUB', 'EUR'];
+
+    private const STANDARDS_EXPECTED = [
+        [
+            AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '643',
+            AttributeCodes::ATTRIBUTE_ALPHA3 => 'RUB',
+            AttributeCodes::ATTRIBUTE_NAME => 'Russian ruble',
+        ],
+        [
+            AttributeCodes::ATTRIBUTE_NUMERIC_CODE => "978",
+            AttributeCodes::ATTRIBUTE_ALPHA3 => "EUR",
+            AttributeCodes::ATTRIBUTE_NAME => "Euro",
+        ],
+    ];
 }
