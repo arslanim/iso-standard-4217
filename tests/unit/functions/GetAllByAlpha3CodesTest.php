0fabe18ea7c123f63b98bca7c615eeff318d86fb
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-10-14 01:21:24 +0400

    Created unit test class.

---
+++ tests/unit/functions/GetAllByAlpha3CodesTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace arslanimamutdinov\ISOStandard4217\tests\unit\functions;
+
+use PHPStan\Testing\TestCase;
+
+/**
+ * @group standard-4217-functions
+ */
+class GetAllByAlpha3CodesTest extends TestCase
+{
+}
